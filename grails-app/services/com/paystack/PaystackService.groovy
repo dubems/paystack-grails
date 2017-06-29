aa2a709ce7201e3d01c2fb6ee4af918ff6f27e78
dubems <nriagudubem@yahoo.com>
2017-06-29 18:30:02 +0100

    development ongoing

---
+++ grails-app/services/com/paystack/PaystackService.groovy
@@ -0,0 +1,75 @@
+package com.paystack
+
+import grails.transaction.Transactional
+
+@Transactional
+class PaystackService {
+
+
+     PaystackService() {
+
+    }
+    /**
+     * Get authorization url from paystack
+     * The authorization url is to redirect to paystack for paymente
+     * @return
+     */
+    def getAuthorizationUrl()
+    {
+        return this.makePaymentRequest()
+    }
+
+    /**
+     * Redirect to the paystack url
+     */
+    def redirectNow()
+    {
+        String url = this.getAuthorizationUrl()
+
+    }
+
+    /**
+     * Validate params being sent to paystack
+     * @param params
+     * @return
+     */
+    def validateParameters(Map params)
+    {
+        if(!params.amount || !params.emamil)
+        {
+            throw new Exception('Incomplete Parameters')
+
+        }
+        return this
+    }
+
+    /**
+     * Make Request to paystack
+     * This returns the authorization url
+     */
+    def makePaymentRequest()
+    {
+        Map reqParams = [
+            amount:params
+        ]
+    }
+
+    /**
+     * Generate a Unique Transaction reference
+     * this is used to verify the transaction
+     * @return
+     */
+    def generateTrxnRef()
+    {
+        List numPool = 0..9
+        List alphaPoolCapital = 'A'..'Z'
+        List alphaPoolSmall   = 'a'..'z'
+
+        List allPool      = (numPool + alphaPoolCapital + alphaPoolSmall)
+        List shuffledPool = Collections.shuffle(allPool)
+        def trxnReference = shuffledPool.subList(0,32)
+
+        return trxnReference
+
+    }
+}
