2bed6d53482e72181428017568b4db5cdd8d7418
dubems <nriagudubem@yahoo.com>
2017-07-10 03:33:13 +0100

    osme few generalizations

--- grails-app/services/com/paystack/PaystackService.groovy
+++ grails-app/services/com/paystack/PaystackService.groovy
@@ -1,6 +1,15 @@
 package com.paystack
 
 import grails.transaction.Transactional
+import grails.util.Environment
+import groovy.json.JsonSlurper
+import org.apache.http.client.CredentialsProvider
+import org.apache.http.client.methods.CloseableHttpResponse
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.BasicCredentialsProvider
+import org.apache.http.impl.client.HttpClientBuilder
+import org.springframework.http.HttpEntity
+import sun.net.www.http.HttpClient
 
 /**
  * @athor Nriagu Chidubem
@@ -9,28 +18,71 @@ import grails.transaction.Transactional
 @Transactional
 class PaystackService {
 
+    String secretKey
+
+    String publicKey
+
+    String endPoint
+
     /**
      * PaystackService Constructor
      */
-     PaystackService() {
+     PaystackService()
+     {
+         setSecretKey()
+         setEndPoint()
+    }
 
+    /**
+     * Set PAYSTACK endpoint
+     */
+    void setEndPoint(){
+        endPoint = grailsApplication.config.paystack.endpoint
+    }
+    /**
+     * Set the secret used for API Request
+     * When app in dev, use the testSecretKey else use the liveSecretKey
+     */
+    void setSecretKey()
+    {
+        if(Environment.current.name == "${Environment.DEVELOPMENT}".toLowerCase() || Environment.current.name == "${Environment.TEST}".toLowerCase()){
+            secretKey =  grailsApplication.config.paystack.testSecretKey
+        }
+        else{
+             secretKey = grailsApplication.config.paystack.liveSecretKey
+        }
+
+    }
+
+    /**
+     * Set the public key for the API Request
+     *  When app in dev, use the testPublicKey else use the livePublicKey
+     */
+    void setPublicKey()
+    {
+        if(Environment.current.name == "${Environment.DEVELOPMENT}".toLowerCase() || Environment.current.name == "${Environment.TEST}".toLowerCase()){
+            publicKey =  grailsApplication.config.paystack.testPublicKey
+        }
+        else{
+            publicKey = grailsApplication.config.paystack.livePublicKey
+        }
     }
     /**
      * Get authorization url from paystack
      * The authorization url is to redirect to paystack for payment
+     * @params params : this contains info to be sent to PAYSTACK(email, amount,...)
      * @return
      */
-    def getAuthorizationUrl()
+    def getAuthorizationUrl(params)
     {
-        return this.makePaymentRequest()
+        return this.makePaymentRequest(params)
     }
 
     /**
      * Redirect to the paystack url
      */
-    def redirectNow()
+    def redirectNow(String url)
     {
-        String url = this.getAuthorizationUrl()
 
     }
 
@@ -44,7 +96,6 @@ class PaystackService {
         if(!params.amount || !params.emamil)
         {
             throw new Exception('Incomplete Parameters')
-
         }
         return this
     }
@@ -52,11 +103,20 @@ class PaystackService {
     /**
      * Make Request to paystack
      * This returns the authorization url
+     * @param  params : THis contains info to be sent PAYSTACK(email,amount,...)
      */
-    def makePaymentRequest()
+    def makePaymentRequest(params)
     {
         Map reqParams = [
-            amount:params
+                amount:params.amount,
+                email:params.email,
+                reference:this.generateTrxnRef(),
+                plan:params.plan,
+                first_name:params.first_name,
+                last_name:params.last_name,
+                metadata:params.metadata,
+                callback_url:params.callback_url
+
         ]
     }
 
@@ -79,24 +139,23 @@ class PaystackService {
 
     }
 
+     def  callAPIEndpoint(String url, Map body,String method)
+    {
+
+    }
+
     /**
      * Verify a transaction
      * @param reference : transaction reference
      * @return : response from paystack
      */
-    Map verify(String reference) {
-        String authString = ""
-        if(Environment.current.name == "${Environment.DEVELOPMENT}".toLowerCase() || Environment.current.name == "${Environment.TEST}".toLowerCase()){
-            authString = "Bearer " + grailsApplication.config.paystack.testSecretKey
-        }
-        else{
-            authString = "Bearer " + grailsApplication.config.paystack.liveSecretKey
-        }
+     Map verify(String reference) {
+
+         String authString = "Bearer" +secretKey
+         String contentType = "application/json"
+         String url = endPoint+"/transaction/verify/${reference}"
         
-        String contentType = "application/json"
-        String url = "${grailsApplication.config.paystack.endpoint}/transaction/verify/${reference}"
-        
-        CredentialsProvider provider = new BasicCredentialsProvider()  
+        CredentialsProvider provider = new BasicCredentialsProvider()
         
         HttpClient client = HttpClientBuilder.create().build()
 
@@ -104,7 +163,7 @@ class PaystackService {
         httpGet.addHeader("Authorization",authString)
 
 
-        CloseableHttpResponse  result = client.execute(httpGet)
+        CloseableHttpResponse result = client.execute(httpGet)
         HttpEntity entity = result.getEntity() // get result
         String responseBody = EntityUtils.toString(entity); // extract response body
         def jsonSlurper = new JsonSlurper() // for parsing response
